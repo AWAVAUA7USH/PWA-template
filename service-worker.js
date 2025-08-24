29d6453803280caf4a29f697a59a22f5e3823252
Giovanni <cacchione.giovanni0@gmail.com>
2025-08-24 21:50:19 +0200

    a

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,15 @@
-const CACHE_NAME = 'minimal-cache-v1';
-const ASSETS_TO_CACHE = [
-  '/index.html',
-  '/index.js',
-  '/service-worker.js'
-];
-
-// Install event – cache only the specified assets
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS_TO_CACHE))
-      .then(() => self.skipWaiting())
+    caches.open('hello-cache').then(cache => cache.addAll([
+      '/index.html',
+      '/manifest.json',
+      '/service-worker.js'
+    ]))
   );
 });
 
-// Activate event – clean up old caches
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-      )
-    ).then(() => self.clients.claim())
-  );
-});
-
-// Fetch event – serve from cache if available
 self.addEventListener('fetch', event => {
-  if (ASSETS_TO_CACHE.includes(new URL(event.request.url).pathname)) {
-    event.respondWith(
-      caches.match(event.request).then(response => response || fetch(event.request))
-    );
-  } else {
-    // Let everything else go to the network
-    event.respondWith(fetch(event.request));
-  }
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
 });
